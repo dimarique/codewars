10ec03cfb68130ba8634bcd6197f7f850ee84862
dimarik <pochtagreen@gmail.com>
2020-10-02 18:47:18 +0300

    conditional statement switch add

---
+++ conditionalStatementSwitch/app.js
@@ -0,0 +1,26 @@
+function howManydays(month) {
+    let days;
+    switch (month) {
+        case 2:
+            days = 28;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            days = 30;
+            break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            days = 31;
+            break;
+        default:
+            return "out of range";
+    }
+    return days;
+}
